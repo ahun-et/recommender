b1e40cf7cf76dc7f7ff19c2d929409da7d1134a7
Simonbelete <simonbelete@gmail.com>
2021-05-04 18:34:17 +0300

    logger and config files

--- initial_recommendation.py
+++ initial_recommendation.py
@@ -1,4 +1,10 @@
 import os
 
+from blender import bcolors
+
 try:
-    print('Connecting to the PostgreSQL')
+    print(f'{bcolors.OKBLUE}INFO: Connecting to the PostgreSQL databse... {bcolors.ENDC}')
+except:
+    except (psycopg2.DatabaseError, psycopg2.OperationalError) as ps_error:
+            print(f'{bcolors.FAIL}ERROR: Failed to connect to PostgreSQL{bcolors.ENDC}')
+            logger.exception(str(ps_error))
